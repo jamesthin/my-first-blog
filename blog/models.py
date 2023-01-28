93635b323334149155a2dab4b0fe42d31111002e
jinsang hwang <jinsang.hwang@gmail.com>
2023-01-28 22:07:26 +0900

    models.py modified

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.db import models
 from django.utils import timezone
+from django.db import models
+from django.db import models
 
 
 class Post(models.Model):
